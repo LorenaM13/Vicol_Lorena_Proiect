e6786f0b9af73045151924b809b6269b2d708776
LORENA-CARMEN VICOL <lorena.vicol@stud.ubbcluj.ro>
2023-11-22 13:44:54 +0000

    Finalizat OK

--- Models/Angajat.cs
+++ Models/Angajat.cs
@@ -5,9 +5,23 @@ namespace Vicol_Lorena_Proiect.Models
     public class Angajat
     {
         public int ID { get; set; }
-        public string NumeAngajat { get; set; }
+
+        public string? NumeAngajat { get; set; }
+
+        /* public string? Prenume { get; set; }
+
+ /*
+         [Display(Name = "Nume Complet")]
+         public string? NumeAngajat
+         {
+             get
+             {
+                 return Nume + " " + Prenume;
+             }
+         }*/
 
         [Display(Name = "NumarContact")]
+        [RegularExpression(@"^\(?(0[0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = "Telefonul trebuie sa fie de forma '0722-222-222'!")]
         public string TelNo { get; set; }
         public string Adresa { get; set; }
 
--- Pages/Angajati/Create.cshtml.cs
+++ Pages/Angajati/Create.cshtml.cs
@@ -28,7 +28,6 @@ namespace Vicol_Lorena_Proiect.Pages.Angajati
         public Angajat Angajat { get; set; } = default!;
         
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
           if (!ModelState.IsValid || _context.Angajat == null || Angajat == null)
